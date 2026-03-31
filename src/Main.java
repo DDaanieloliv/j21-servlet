b4b0da1f8e72fffe77ae64487311e4f07ce1765e
ddaaniel <daniel0333v@gmail.com>
2026-03-31 16:19:46 -0300

    improving the byte parsing to header's protocol

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -13,7 +12,6 @@ public class Main {
 	public static int indexOf(byte[] bytes, int character) {
 		for (int i = 0; i < bytes.length; i++)
 			if (bytes[i] == character) return i;
-
 		return -1;
 	}
 
@@ -22,35 +20,23 @@ public class Main {
 
 		Path path = Paths.get("/home/daniel/personal/tmp/httpfromtcp/messages.txt");
 		InputStream st = Files.newInputStream(path);
-		// FileInputStream st = new FileInputStream(path.toFile());
 
-		byte[] part = new byte[8];
 		String line = new String();
-		byte[] byteChunk = new byte[7];
-
-		var partPreview = new char[8];
+		byte[] part = new byte[8];
 
 		for (;;) {
 			if (st.read(part) == -1) break;
-			String tempStr = new String(part, StandardCharsets.UTF_8);
-		 
-			for ( int i = 0; i < 8; i++) {
-				partPreview[i] = (char) part[i];
-			}
+			int posBckSlash = indexOf(part, 10);
       
-			if (indexOf(part, 10) != -1) {
-				byte[] byteArr = Arrays.copyOf(part, indexOf(part, 10));
-				tempStr = new String(byteArr, StandardCharsets.UTF_8);
-				line = line.concat(tempStr);
+			if (posBckSlash != -1) {
+				byte[] bytesBehind = Arrays.copyOf(part, posBckSlash);
+				byte[] bytesAhead = Arrays.copyOfRange(part, posBckSlash + 1, part.length);
+				
+				line = line.concat(new String(part, 0, posBckSlash, StandardCharsets.UTF_8));
 				System.out.printf("read: %s\n", line);
-				tempStr = "";
-				line = "";
+				line = "".concat(new String(part, posBckSlash + 1, (part.length - 1) - posBckSlash, StandardCharsets.UTF_8));
 			}
-			else { 
-				line = line.concat(tempStr);
-			}
-
-			if (st.available() == 0) break;
+			else line = line.concat(new String(part));
 		}
 		st.close();
 	}
